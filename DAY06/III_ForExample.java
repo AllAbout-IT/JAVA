757b7587dc43a37a14dff6423be5fd3d6691f757
jayilent <bs.junesei@gmail.com>
2022-12-13 23:36:07 +0900

    221213

---
+++ DAY06/III_ForExample.java
@@ -0,0 +1,26 @@
+public class III_ForExample {
+  public static void main(String[] args) {
+    /*
+     반복문 for
+      - for문은 제어조건을 한꺼번에 지정한다는 점이 다른 반복문과는 다릅니다.
+      - 따라서 정확한 반복횟수를 알고 있을 때는 for문이 while문 보다 유용합니다.
+      - for문도 다른 제어문과 같이 실행문장이 한불이면 블록 {}을 생략할 수 있습니다.
+      - for문의 구조
+          for(초기값; boolean형 조건식; 반복 표현식) {
+            반복할 실행문; 
+          }
+     */
+    /*
+    [while문]
+     제어번수 선언(begin)
+     while (논리형 조건식(end)
+      반복실행할 코드
+      증김식(stop)
+
+    [for문]
+     for(제어변수선언(begin); 논리형조건식(end); 증감식(stop)) {
+      반복실행할 코드
+     }
+     */
+  }
+}
---
+++ DAY06/II_DoWhileExample.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class II_DoWhileExample  {
+  public static void main(String[] args) {
+    // 정수를 입력받아 1부터 입력받은 숫자까지의 누적합을 구하는 로직
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("정수: ");
+    int target = scan.nextInt(); // target 임력 받은 임의의 정수
+    int sum = 0;
+    int n = 1;
+
+    // [while문 만을 사용했을 경우]
+    // target이 '0'일때 false가 되어 제대로 수행되지 않는다.
+    //
+    // while(n <= target) {
+    //   sum += n; // sum = sum + n
+    //   n++; // 후위연산자로 다음의 n에 1이 증가
+    // }
+
+    // [do ~ while을 사용했을 경우]
+    // target이 0이어도 do문을 통해 일단 한번은 실행이 되고 다음 while의 조건에 의해서 반복문의 실행여부를 결정한다.
+    do {
+      sum += n;
+      n++;
+    }while(n <= target);
+
+    /*
+    [while문]
+     제어번수 선언(begin)
+     while (논리형 조건식(end)
+      반복실행할 코드
+      증김식(stop)
+
+    [for문]
+     */
+
+    System.out.println("1부터 " + target + "까지의 누적합계" + sum);
+    scan.close();
+  }
+
+}
