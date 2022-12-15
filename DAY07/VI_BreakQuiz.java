6c3eb049f9b311717b986d463c2fcdd03feee9c0
jayilent <bs.junesei@gmail.com>
2022-12-15 22:38:16 +0900

    221215

---
+++ DAY07/VI_BreakQuiz.java
@@ -0,0 +1,11 @@
+public class VI_BreakQuiz {
+  public static void main(String[] args) {
+    /*
+     * 1. 두수의 더하기를 맞추는 문제를 지속적으로 출제하는 프로그램입니다.
+     * 2. 2가지의 정수를 1~100사이의 난수를 발생 시켜 지속적으로 문제를 출제한 후 정답을 맞추면 반복문을 탈출시키세요.
+     * 3. 27 + 84
+     */
+
+     Int i = (int)(Math.random() * 100);
+  }
+}
